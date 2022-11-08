12f26e25267044240f15c6d0435e1c707bf30c8c
vzsoares <jogolots2@gmail.com>
2022-11-08 20:28:05 -0300

    initiate login template

--- DreamTODO/urls.py
+++ DreamTODO/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from pages.views import home_view, login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('login/', login_view, name='login')
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 
 def home_view(request, *args, **kwargs):
     return render(request,'home.html',{})
+
+def login_view(request, *args, **kwargs):
+    return render(request,'login.html',{})
