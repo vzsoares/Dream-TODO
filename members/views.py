88d9d597e654183028a17406fc08eaf4a6f73ae8
vzsoares <jogolots2@gmail.com>
2022-11-09 20:05:14 -0300

    add message

--- members/views.py
+++ members/views.py
@@ -6,4 +6,15 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, 'login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, 'There was an error login in')
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
