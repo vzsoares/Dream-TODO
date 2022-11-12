178007d990b90aada58b59d3f6154e2e5eab8343
vzsoares <jogolots2@gmail.com>
2022-11-12 14:01:43 -0300

    schetch model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+def List():
+    list = ['list of obj, index sort']
+    obj = {
+        'id': 'id',
+        'title': 'titleTXT',
+        'description': 'descriptionTXT',
+        'done': 'boolean',
+    }
+    user = {
+        'id': 'id references user ID',
+        'todos': 'list'
+    }
