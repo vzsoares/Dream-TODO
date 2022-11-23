88c7239c95adc58faf1f698f7e623d7276fd70b4
vzsoares <jogolots2@gmail.com>
2022-11-22 21:01:43 -0300

    initialize models

--- todo/models.py
+++ todo/models.py
@@ -1,17 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
-def List():
-    list = ['list of obj, index sort']
-    obj = {
-        'id': 'id',
-        'title': 'titleTXT',
-        'description': 'descriptionTXT',
-        'done': 'boolean',
-    }
-    user = {
-        'id': 'id references user ID',
-        'todos': 'json.stringfy(list)'
-    }
+class UserTodo(models.Model):
+    owner = models.OneToOneField(
+        User, on_delete=models.DO_NOTHING, primary_key=True)
+    todos = models.TextField()
+
+    def __str__(self):
+        return self.owner.username
